a6feb8ffacd390b52a368957bacaa283a6164f45
TKUEE-Ja <105557092+TKUEE-Ja@users.noreply.github.com>
2023-05-01 00:00:44 +0800

    Update P03.cpp

--- P03.cpp
+++ P03.cpp
@@ -1,48 +1,35 @@
-#include <stdio.h>
-#include <ctime>
-
-#define IN "P03IN.txt"
-#define OUT "P03OUT.txt"
-
-char table[] = "`1234567890-=QWERTYUIOP[]\\ASDFGHJKL;'ZXCVBNM,./";
+#include<iostream>
+#include<stdio.h>
+#include<vector>
+using namespace std;
 
 int main()
 {
-	freopen(IN, "r", stdin);
-	freopen(OUT, "w", stdout);
-	clock_t cS = clock();
+	freopen("P03IN.txt", "r", stdin);
+	freopen("P03OUT.txt", "w", stdout);
 
-	int iChar;
-	bool bFlag;
-
-	while ((iChar = getchar()) != EOF)
+	char a;
+	char s[] = "`1234567890-=QWERTYUIOP[]ASDFGHJKL;'\\ZXCVBNM,./";
+	while (cin.get(a))				//WARNING!! cin can't get the space
 	{
-		bFlag = true;
-		for (int i = 0; table[i] && bFlag; i++)
+		if (a == ' ')
 		{
-			if (iChar == ' ')
+			cout << " ";
+		}
+		else
+		{
+			for (int i = 0; i < sizeof s / sizeof s[0]; i++)
 			{
-				bFlag = false;
-				putchar(iChar);
+				if (a == s[i])
+				{
+					cout << s[i - 1];
+				}
 			}
-			else if (iChar == table[i])
-			{
-				bFlag = false;
-				putchar(table[i - 1]);
-			}
-			else
-				continue;
-
 		}
 	}
 
-
-
-	clock_t cE = clock();
 	freopen("CON", "r", stdin);
 	freopen("CON", "w", stdout);
-	double cost = (double)(cE - cS) / CLK_TCK;
-	printf("Cost time: %.2f\n", cost);
 
 	return 0;
 }
